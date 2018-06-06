5bb9d7d338089fb20cb8055c6d3ef643b7ad1d7f
Author Name <ivan-.-sotelo@hotmail.com>
2018-06-06 13:40:44 -0600

    Template

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -8,12 +8,18 @@ module.exports = {
   env: {
     browser: true,
   },
-  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-  extends: ['plugin:vue/essential', 'airbnb-base'],
-  // required to lint *.vue files
+  {{#if_eq eslintConfig 'standard'}}
+  extends: 'standard',
+  {{/if_eq}}
+  {{#if_eq eslintConfig 'airbnb'}}
+  extends: 'airbnb-base',
+  {{/if_eq}}
+  globals: {
+    __static: true
+  },
   plugins: [
-    'vue',
+    'html',
+    'vue'
   ],
   // check if imports actually resolve
   settings: {
@@ -23,8 +29,14 @@ module.exports = {
       }
     }
   },
-  // add your custom rules here
   rules: {
+    {{#if_eq eslintConfig 'standard'}}
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    {{/if_eq}}
+    {{#if_eq eslintConfig 'airbnb'}}
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       js: 'never',
@@ -40,10 +52,22 @@ module.exports = {
         'e' // for e.returnvalue
       ]
     }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      optionalDependencies: ['test/unit/index.js']
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': ['error', {
+      props: true,
+      ignorePropertyModificationsFor: [
+        'state', // for vuex state
+        'acc', // for reduce accumulators
+        'e' // for e.returnvalue
+      ]
     }],
+    'no-shadow': 0,
+    'import/extensions': 0,
+    'import/newline-after-import': 0,
+    'no-multi-assign': 0,
+    {{/if_eq}}
     // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
